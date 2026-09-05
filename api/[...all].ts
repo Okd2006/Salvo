aea71352116156309801d5b55a4c85a14c5b160c
Omkrrish <omkrrishbgs@gmail.com>
2026-09-05 12:20:57 +0530

    fix(vercel): route all /api/* requests to api/index.ts with match parameter and remove redundant [...all].ts

--- api/[...all].ts
+++
@@ -1,6 +0,0 @@
-import type { IncomingMessage, ServerResponse } from 'node:http';
-import { handleApiRequest } from '../src/api/handler.js';
-
-export default async function handler(req: IncomingMessage, res: ServerResponse): Promise<void> {
-  await handleApiRequest(req, res);
-}
